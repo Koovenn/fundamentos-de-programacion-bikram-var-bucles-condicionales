8ea7984b6d03aaaed2407ad52c81062d411f1e77
Koovenn <educanaless@gmail.com>
2023-09-25 21:02:07 +0100

    Progreso_Bikrams

---
+++ index.js
@@ -0,0 +1,43 @@
+//Variables
+
+let variableSinValor;
+
+let booleano1 = (2 > 1);
+let booleano2 = (1 > 5);
+
+const PI = 3.14;
+
+const TAU = 2 * PI;
+
+//Booleanos
+
+let booleanoAnd = (2 > 1 && 1 > 5);
+
+let booleanoNot = !(2 > 1);
+
+let booleanoMix0 = ((2 > 1 || 1 > 5) && (2 > 1 || !(2 > 1) && !(1 > 5)));
+
+//Operadores
+
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+
+//Bucles
+
+let contarHasta10_2 = 0;
+
+for (contarHasta10_2 = 0; contarHasta10_2 < 10; contarHasta10_2++) {
+    if (contarHasta10_2 === 10) {
+    console.log(contarHasta10_2);
+    }
+}
+
+let postI = 0;
+let postJ = 0;
+
+for (postI = 0; postI < 11; postI++) {
+    return ;
+}
